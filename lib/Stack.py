bae13df49b42f20b4c11bcb0802b61cc42843672
Prabhdip Gill <prabhdip.gill@flatironschool.com>
2022-10-23 22:32:58 -0700

    Add main branch

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,42 +1,26 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        self.items = []
-        self.limit = limit
-
-        for item in items:
-            if(not self.full()):
-                self.items.append(item)
+        pass
 
 
     def isEmpty(self):
-        return self.items == []
+        pass
 
     def push(self, item):
-        if (not self.full()):
-            self.items.insert(0, item)
-        else:
-            return None
+        pass
 
     def pop(self):
-        if self.isEmpty():
-            return None
-        return self.items.pop(0)
+        pass
 
     def peek(self):
-	    return self.items[0]
+	    pass
 
     def size(self):
-        return len(self.items)
+        pass
 
     def full(self):
-
-        if (len(self.items) >= self.limit):
-            return True
-        return False
+        pass
 
     def search(self, target):
-        for i in range(len(self.items)):
-            if self.items[i] == target:
-                return i
-        return -1
+        pass
